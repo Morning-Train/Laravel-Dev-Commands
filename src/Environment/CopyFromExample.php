54882096bc15b721109ffaf9007c17df710c72ad
Bjarne <bb@morningtrain.dk>
2020-04-29 08:00:57 +0200

    Initial commit

---
+++ src/Environment/CopyFromExample.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace MorningTrain\Laravel\Dev\Commands\Environment;
+
+use Illuminate\Console\Command;
+
+class CopyFromExample extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'env:copy-from-example {--force}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Assists making the .env file';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if ($this->option('force') || $this->confirm('WARNING: This will clear the DATABASE. Are you sure you want to continue?')) {
+
+            $env_path = base_path('.env');
+            $env_example_path = base_path('.env.example');
+
+            if(!file_exists($env_example_path)) {
+                throw new \Exception('Example .env file does not exist in project root (.env.example)');
+            }
+
+            if(!file_exists($env_path) || $this->shouldForceCopy()) {
+                copy($env_example_path, $env_path);
+            }
+
+        }
+    }
+
+    protected function shouldForceCopy()
+    {
+        return $this->option('force') || $this->confirm('WARNING: An .env file already exists, would you like to replace it anyways?');
+    }
+
+}
---
+++ src/LaravelDevCommandsServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MorningTrain\Laravel\Dev\Commands;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelDevCommandsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Environment\CopyFromExample::class,
+            ]);
+        }
+    }
+
+}
